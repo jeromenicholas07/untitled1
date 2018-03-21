53e63cb9f451112b4aa4e3f36e8856fb87107ebb
jeromenicholas <jeromenicholas07@gmail.com>
2018-03-21 12:12:44 +0530

    urls error updated

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
-
+    path('', include('blog.urls')),
 
 ]
