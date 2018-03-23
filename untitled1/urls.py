5217957c35c96978cb320ff042642767f584b990
jeromenicholas <jeromenicholas07@gmail.com>
2018-03-24 00:28:02 +0530

    bad page and logo doing

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import patterns as patterns
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.conf.urls import url
+from django.conf import settings
+import os
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('', include('blog.urls')),
+    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
