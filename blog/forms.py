60ad663b15b352e3ec3cc990f73d467bf2dcf9ad
jeromenicholas <jeromenicholas07@gmail.com>
2018-04-07 18:29:19 +0530

    sentiment anal updated

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,5 @@ class ttsForm(forms.Form):
     txt = forms.CharField(label="", max_length=500, widget=forms.Textarea(attrs={'class': 'form-control justify-content-start', 'style': 'height: 250px; padding-bottom: 60px; margin-botton: 0', 'id': 'txt', 'placeholder': 'Enter text here.'}))
 
 class sentiForm(forms.Form):
-    txt = forms.CharField(label="", max_length=300, widget=forms.Textarea(attrs={'class': 'form-control justify-content-start', 'style': 'height: 250px; padding-bottom: 60px; margin-botton: 0', 'id': 'txt', 'placeholder': 'Enter text here.', 'pattern': ".{25,}"}))
+    txt = forms.CharField(label="", max_length=300, min_length=25, widget=forms.Textarea(attrs={'class': 'form-control justify-content-start', 'style': 'height: 250px; padding-bottom: 60px; margin-botton: 0', 'id': 'txt', 'placeholder': 'Enter text here.', 'pattern': ".{25,}"}))
 
